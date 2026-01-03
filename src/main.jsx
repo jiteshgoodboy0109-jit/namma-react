96aafd57b70d9cd59bb7756fc234ba56440f1f5b
jitesh <jiteshgoodboy.0109@gmail.com>
2026-01-04 01:59:27 +0530

    ji1

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/namma-react/">
+    <BrowserRouter >
       <ToastProvider>
         <App />
       </ToastProvider>
