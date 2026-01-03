37f2224e74c01e08c7466fe4f8f3068ca71e90d3
jitesh <jiteshgoodboy.0109@gmail.com>
2026-01-04 01:54:41 +0530

    jit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App, { ToastProvider } from './App.jsx'
 import './index.css'
-
+import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/namma-react/">
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
